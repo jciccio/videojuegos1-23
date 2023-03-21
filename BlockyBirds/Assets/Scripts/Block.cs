6739f4699e9c3c701d16527e39092fb13d32d95b
Jose Ciccio <jciccio@gmail.com>
2023-03-21 09:16:47 -0600

    Ejemplo de Blocky Birds agregado al repo con manejo de marcador, colisiones y jugador

--- BlockyBirds/Assets/Scripts/Block.cs
+++ BlockyBirds/Assets/Scripts/Block.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] private float Velocity = 1f;
+    [SerializeField] private float VelocityChange = 0.25f;
+
+    private float StartY;
+
+    public static int Score = 0;
+    
+
+    
     //Start is called before the first frame update
     void Start()
     {
-        
+        StartY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.left * Time.deltaTime * Velocity;  
+
+        // Si la posicion del bloque es < -10 => 21f
+        if(transform.position.x < -10f){
+            float yPos = StartY + UnityEngine.Random.Range(-1f,1f);
+
+            transform.position = new Vector3(transform.position.x + 21f, yPos, transform.position.z);
+            Velocity += VelocityChange;
+            Score++;
+        }
     }
 
 }
---
+++ BlockyBirds/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    
+    [SerializeField] Vector2 Jump = Vector2.up; // 0,1
+
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            rb.velocity = Jump;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag(Constants.DEATH_TAG)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
